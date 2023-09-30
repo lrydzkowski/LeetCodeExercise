16aa0842a7200258855b9836e905690ce5558d7d
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2023-09-30 12:48:50 +0200

    feat: exercise 225

---
+++ LeetCodeExercise.Core/Queues/Exercise225.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCodeExercise.Core.Queues;
+
+// https://leetcode.com/problems/implement-stack-using-queues/
+public class Exercise225
+{
+    private readonly Queue<int>[] _queues = { new(), new() };
+
+    public void Push(int x)
+    {
+        (Queue<int> empty, Queue<int> filled) =
+            _queues[0].Count == 0 ? (_queues[0], _queues[1]) : (_queues[1], _queues[0]);
+        empty.Enqueue(x);
+        while (filled.Count > 0)
+        {
+            empty.Enqueue(filled.Dequeue());
+        }
+    }
+
+    public int Pop()
+    {
+        Queue<int> filled = GetFilledQueue();
+
+        return filled.Dequeue();
+    }
+
+    public int Top()
+    {
+        Queue<int> filled = GetFilledQueue();
+
+        return filled.Peek();
+    }
+
+    public bool Empty()
+    {
+        return _queues[0].Count == 0 && _queues[1].Count == 0;
+    }
+
+    private Queue<int> GetFilledQueue()
+    {
+        Queue<int> filled = _queues[0].Count == 0 ? _queues[1] : _queues[0];
+        if (filled.Count == 0)
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+
+        return filled;
+    }
+}
---
+++ LeetCodeExercise.Tests/Queues/Exercise225Tests.cs
@@ -0,0 +1,18 @@
+﻿using LeetCodeExercise.Core.Queues;
+
+namespace LeetCodeExercise.Tests.Queues;
+
+public class Exercise225Tests
+{
+    [Fact]
+    public void Test()
+    {
+        Exercise225 exercise = new();
+        exercise.Push(1);
+        exercise.Push(2);
+
+        Assert.Equal(2, exercise.Top());
+        Assert.Equal(2, exercise.Pop());
+        Assert.False(exercise.Empty());
+    }
+}
