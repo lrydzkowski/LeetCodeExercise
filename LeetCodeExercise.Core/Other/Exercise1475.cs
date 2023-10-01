5106d3bba8ed274719822e46b94b537385e4f6a0
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2023-10-01 07:57:38 +0200

    feat: exercise 1475

---
+++ LeetCodeExercise.Core/Other/Exercise1475.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeExercise.Core.Other;
+
+// <https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/>
+public class Exercise1475
+{
+    public static int[] Execute(int[] prices)
+    {
+        for (int i = 0; i < prices.Length - 1; i++)
+        {
+            for (int j = i + 1; j < prices.Length; j++)
+            {
+                if (prices[j] <= prices[i])
+                {
+                    prices[i] -= prices[j];
+
+                    break;
+                }
+            }
+        }
+
+        return prices;
+    }
+}
---
+++ LeetCodeExercise.Tests/Other/Exercise1475Tests.cs
@@ -0,0 +1,17 @@
+﻿using LeetCodeExercise.Core.Other;
+
+namespace LeetCodeExercise.Tests.Other;
+
+public class Exercise1475Tests
+{
+    [Theory]
+    [InlineData(new[] { 8, 4, 6, 2, 3 }, new[] { 4, 2, 4, 2, 3 })]
+    [InlineData(new[] { 1, 2, 3, 4, 5 }, new[] { 1, 2, 3, 4, 5 })]
+    [InlineData(new[] { 10, 1, 1, 6 }, new[] { 9, 0, 1, 6 })]
+    public void Test(int[] prices, int[] expectedResult)
+    {
+        int[] result = Exercise1475.Execute(prices);
+
+        Assert.Equal(expectedResult, result);
+    }
+}
