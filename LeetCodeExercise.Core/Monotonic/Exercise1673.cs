3d6e5a599b74a4a63bdbeb3fd3fe75486ea12efb
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2023-10-01 09:54:27 +0200

    feat: exercise 1673

---
+++ LeetCodeExercise.Core/Monotonic/Exercise1673.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCodeExercise.Core.Monotonic;
+
+public static class Exercise1673
+{
+    public static int[] ExecuteSlow(int[] numbers, int k)
+    {
+        Stack<int> monotonicStack = new();
+
+        int index = 0;
+        for (int i = 0; i < k; i++)
+        {
+            for (int j = index; j < numbers.Length - k + i + 1; j++)
+            {
+                int number = numbers[j];
+                while (monotonicStack.Count > i && monotonicStack.Peek() > number)
+                {
+                    monotonicStack.Pop();
+                }
+
+                if (monotonicStack.Count < i + 1)
+                {
+                    monotonicStack.Push(number);
+                    index = j + 1;
+                }
+            }
+        }
+
+        return monotonicStack.ToArray().Reverse().ToArray();
+    }
+
+    public static int[] ExecuteFast(int[] numbers, int k)
+    {
+        Stack<int> monotonicStack = new();
+
+        for (int j = 0; j < numbers.Length; j++)
+        {
+            int number = numbers[j];
+            while (monotonicStack.Count > 0
+                   && monotonicStack.Peek() > number
+                   && numbers.Length - j > k - monotonicStack.Count)
+            {
+                monotonicStack.Pop();
+            }
+
+            if (monotonicStack.Count < k)
+            {
+                monotonicStack.Push(number);
+            }
+        }
+
+        return monotonicStack.ToArray().Reverse().ToArray();
+    }
+}
---
+++ LeetCodeExercise.Tests/Monotonic/Exercise1673Tests.cs
@@ -0,0 +1,30 @@
+﻿using LeetCodeExercise.Core.Monotonic;
+
+namespace LeetCodeExercise.Tests.Monotonic;
+
+public class Exercise1673Tests
+{
+    [Theory]
+    [InlineData(new int[] { 3, 5, 2, 6 }, 2, new int[] { 2, 6 })]
+    [InlineData(new int[] { 2, 4, 3, 3, 5, 4, 9, 6 }, 4, new int[] { 2, 3, 3, 4 })]
+    [InlineData(new int[] { 3, 5, 2, 1 }, 2, new int[] { 2, 1 })]
+    [InlineData(new int[] { 71, 18, 52, 29, 55, 73, 24, 42, 66, 8, 80, 2 }, 3, new int[] { 8, 80, 2 })]
+    public void TestSlow(int[] numbers, int k, int[] expectedResult)
+    {
+        int[] result = Exercise1673.ExecuteSlow(numbers, k);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Theory]
+    [InlineData(new int[] { 3, 5, 2, 6 }, 2, new int[] { 2, 6 })]
+    [InlineData(new int[] { 2, 4, 3, 3, 5, 4, 9, 6 }, 4, new int[] { 2, 3, 3, 4 })]
+    [InlineData(new int[] { 3, 5, 2, 1 }, 2, new int[] { 2, 1 })]
+    [InlineData(new int[] { 71, 18, 52, 29, 55, 73, 24, 42, 66, 8, 80, 2 }, 3, new int[] { 8, 80, 2 })]
+    public void TestFast(int[] numbers, int k, int[] expectedResult)
+    {
+        int[] result = Exercise1673.ExecuteFast(numbers, k);
+
+        Assert.Equal(expectedResult, result);
+    }
+}
