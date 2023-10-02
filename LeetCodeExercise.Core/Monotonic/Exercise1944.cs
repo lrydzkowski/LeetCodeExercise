333a28635e636bddee9f1210924571cdcc69cbb3
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2023-10-02 18:20:25 +0200

    feat: add slow solution for exercise no 1944

---
+++ LeetCodeExercise.Core/Monotonic/Exercise1944.cs
@@ -0,0 +1,77 @@
+﻿namespace LeetCodeExercise.Core.Monotonic;
+
+// https://leetcode.com/problems/number-of-visible-people-in-a-queue/description/
+public static class Exercise1944
+{
+    public static int[] RunSlow(int[] heights)
+    {
+        int[] results = new int[heights.Length];
+        Dictionary<int, Stack<int>> monotonic = new();
+        for (int index = 1; index < heights.Length; index++)
+        {
+            int height = heights[index];
+
+            List<int> keysToRemove = new();
+            foreach (KeyValuePair<int, Stack<int>> element in monotonic)
+            {
+                if (heights[element.Value.Peek()] < height)
+                {
+                    element.Value.Push(index);
+                }
+
+                if (heights[element.Key] < height)
+                {
+                    results[element.Key] = element.Value.Count;
+                    keysToRemove.Add(element.Key);
+                }
+            }
+
+            foreach (int keyToRemove in keysToRemove)
+            {
+                monotonic.Remove(keyToRemove);
+            }
+
+            if (heights[index - 1] <= height)
+            {
+                results[index - 1] = 1;
+            }
+            else
+            {
+                monotonic[index - 1] = new Stack<int>();
+                monotonic[index - 1].Push(index);
+            }
+        }
+
+        foreach (KeyValuePair<int, Stack<int>> element in monotonic)
+        {
+            results[element.Key] = element.Value.Count;
+        }
+
+        return results;
+    }
+
+    public static int[] RunFast(int[] heights)
+    {
+        int[] results = new int[heights.Length];
+        Stack<int> mnemonic = new();
+        for (int index = 0; index < heights.Length; index++)
+        {
+            int height = heights[index];
+            while (mnemonic.Count > 0 && heights[mnemonic.Peek()] < height)
+            {
+                int previousIndex = mnemonic.Pop();
+                results[previousIndex] = index - previousIndex;
+            }
+
+            mnemonic.Push(index);
+        }
+
+        while (mnemonic.Count > 0)
+        {
+            int previousIndex = mnemonic.Pop();
+            results[previousIndex] = heights.Length - 1 - previousIndex;
+        }
+
+        return results;
+    }
+}
---
+++ LeetCodeExercise.Tests/Monotonic/Exercise1944Tests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeExercise.Core.Monotonic;
+
+namespace LeetCodeExercise.Tests.Monotonic;
+
+public class Exercise1944Tests
+{
+    [Theory]
+    [InlineData(new[] { 10, 6, 8, 5, 11, 9 }, new[] { 3, 1, 2, 1, 1, 0 })]
+    [InlineData(new[] { 5, 1, 2, 3, 10 }, new[] { 4, 1, 1, 1, 0 })]
+    public void TestSlow(int[] heights, int[] expectedResult)
+    {
+        int[] result = Exercise1944.RunSlow(heights);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Theory]
+    [InlineData(new[] { 10, 6, 8, 5, 11, 9 }, new[] { 3, 1, 2, 1, 1, 0 })]
+    //[InlineData(new[] { 5, 1, 2, 3, 10 }, new[] { 4, 1, 1, 1, 0 })]
+    public void TestFast(int[] heights, int[] expectedResult)
+    {
+        int[] result = Exercise1944.RunFast(heights);
+
+        Assert.Equal(expectedResult, result);
+    }
+}
