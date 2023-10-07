b86b92ce5bd5737a52855eb3ce8136cb92278512
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2023-10-07 08:39:44 +0200

    feat: exercise 111

---
+++ LeetCodeExercise.Core/Trees/Exercise111.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCodeExercise.Core.Trees;
+
+public static class Exercise111
+{
+    public static int MinDepth(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        int min = FindMinDepth(root);
+
+        return min;
+    }
+
+    private static int FindMinDepth(TreeNode? node, int current = 0, int min = int.MaxValue)
+    {
+        if (node == null)
+        {
+            return min;
+        }
+
+        current++;
+
+        if (node.Left == null && node.Right == null)
+        {
+            min = Math.Min(current, min);
+
+            return min;
+        }
+
+        int leftMin = FindMinDepth(node.Left, current, min);
+        int rightMin = FindMinDepth(node.Right, current, min);
+
+        return Math.Min(leftMin, rightMin);
+    }
+}
+
+public class TreeNode
+{
+    public int Val { get; }
+    public TreeNode? Left { get; }
+    public TreeNode? Right { get; }
+
+    public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+    {
+        Val = val;
+        Left = left;
+        Right = right;
+    }
+}
---
+++ LeetCodeExercise.Tests/Trees/Exercise111Tests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCodeExercise.Core.Trees;
+
+namespace LeetCodeExercise.Tests.Trees;
+
+public class Exercise111Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(TreeNode? root, int expectedResult)
+    {
+        int result = Exercise111.MinDepth(root);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object?[]> GetParameters()
+    {
+        yield return new object?[]
+            { new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7))), 2 };
+        yield return new object?[] { null, 0 };
+    }
+}
