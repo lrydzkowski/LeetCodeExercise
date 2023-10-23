5b6b667b21879c85e6e1a1feb3500c59f98605a4
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2023-10-23 19:54:23 +0200

    feat: exercise 543

---
+++ LeetCodeExercise.Core/Trees/Exercise543.cs
@@ -0,0 +1,44 @@
+﻿using LeetCodeExercise.Core.Common;
+
+namespace LeetCodeExercise.Core.Trees;
+
+public static class Exercise543
+{
+    public static int DiameterOfBinaryTree(TreeNode? root)
+    {
+        int diameter = FindDiameter(root);
+
+        return diameter;
+    }
+
+    private static int FindDiameter(TreeNode? root, int diameter = 0)
+    {
+        if (root == null)
+        {
+            return diameter;
+        }
+
+        int currentDiameter = FindDepth(root.Left) + FindDepth(root.Right);
+        diameter = Math.Max(diameter, currentDiameter);
+
+        diameter = FindDiameter(root.Left, diameter);
+        diameter = FindDiameter(root.Right, diameter);
+
+        return diameter;
+    }
+
+    private static int FindDepth(TreeNode? root, int depth = 0)
+    {
+        if (root == null)
+        {
+            return depth;
+        }
+
+        depth++;
+
+        int leftDepth = FindDepth(root.Left, depth);
+        int rightDepth = FindDepth(root.Right, depth);
+
+        return Math.Max(leftDepth, rightDepth);
+    }
+}
---
+++ LeetCodeExercise.Tests/Trees/Exercise543Tests.cs
@@ -0,0 +1,44 @@
+﻿using LeetCodeExercise.Core.Common;
+using LeetCodeExercise.Core.Trees;
+
+namespace LeetCodeExercise.Tests.Trees;
+
+public class Exercise543Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(TreeNode root, int expectedResult)
+    {
+        int result = Exercise543.DiameterOfBinaryTree(root);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return new object[]
+        {
+            new TreeNode(
+                4,
+                new TreeNode(-7),
+                new TreeNode(
+                    -3,
+                    new TreeNode(
+                        -9,
+                        new TreeNode(
+                            9,
+                            new TreeNode(6, new TreeNode(0, null, new TreeNode(-1)), new TreeNode(6, new TreeNode(-4)))
+                        ),
+                        new TreeNode(
+                            -7,
+                            new TreeNode(-6, new TreeNode(5)),
+                            new TreeNode(-6, new TreeNode(9, new TreeNode(-2)))
+                        )
+                    ),
+                    new TreeNode(-3, new TreeNode(-4))
+                )
+            ),
+            8
+        };
+    }
+}
