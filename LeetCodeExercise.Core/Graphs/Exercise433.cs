c03d6d3568f544a45b68bf6c0ed8a93bc701b830
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-01-14 07:13:59 +0100

    feat: exercise 433

---
+++ LeetCodeExercise.Core/Graphs/Exercise433.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+
+namespace LeetCodeExercise.Core.Graphs;
+
+public static class Exercise433
+{
+    private static readonly char[] AllowedCharacters = ['A', 'C', 'G', 'T'];
+
+    public static int MinMutation(string startGene, string endGene, string[] bank)
+    {
+        HashSet<string> bankSet = [..bank];
+        Dictionary<string, bool> visited = new() { [startGene] = true };
+        Queue<string> genesToAnalyze = new();
+        genesToAnalyze.Enqueue(startGene);
+        int steps = 0;
+        while (genesToAnalyze.Count > 0)
+        {
+            int genesToAnalyzeCount = genesToAnalyze.Count;
+            for (int i = 0; i < genesToAnalyzeCount; i++)
+            {
+                string geneToAnalyze = genesToAnalyze.Dequeue();
+                if (geneToAnalyze == endGene)
+                {
+                    return steps;
+                }
+
+                AddNextGenesToQueue(geneToAnalyze, bankSet, visited, genesToAnalyze);
+            }
+
+            steps++;
+        }
+
+        return -1;
+    }
+
+    private static void AddNextGenesToQueue(
+        string gene,
+        HashSet<string> bankSet,
+        Dictionary<string, bool> visited,
+        Queue<string> genesToAnalyze
+    )
+    {
+        for (int i = 0; i < gene.Length; i++)
+        {
+            char geneLetter = gene[i];
+            foreach (char allowedCharacter in AllowedCharacters)
+            {
+                if (geneLetter == allowedCharacter)
+                {
+                    continue;
+                }
+
+                StringBuilder newGeneBuilder = new(gene)
+                {
+                    [i] = allowedCharacter
+                };
+                string newGene = newGeneBuilder.ToString();
+                if (!bankSet.Contains(newGene))
+                {
+                    continue;
+                }
+
+                if (!visited.TryAdd(newGene, true))
+                {
+                    continue;
+                }
+
+                genesToAnalyze.Enqueue(newGene);
+            }
+        }
+    }
+}
---
+++ LeetCodeExercise.Tests/Trees/Exercise433Tests.cs
@@ -0,0 +1,42 @@
+﻿using LeetCodeExercise.Core.Graphs;
+
+namespace LeetCodeExercise.Tests.Trees;
+
+public class Exercise433Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(string startGene, string endGene, string[] bank, int expectedResult)
+    {
+        int result = Exercise433.MinMutation(startGene, endGene, bank);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            "AACCGGTT",
+            "AACCGGTA",
+            new[] { "AACCGGTA" },
+            1
+        ];
+
+        yield return
+        [
+            "AACCGGTT",
+            "AAACGGTA",
+            new[] { "AACCGGTA", "AACCGCTA", "AAACGGTA" },
+            2
+        ];
+
+        yield return
+        [
+            "AACCGGTT",
+            "AACCGGTA",
+            Array.Empty<string>(),
+            -1
+        ];
+    }
+}
