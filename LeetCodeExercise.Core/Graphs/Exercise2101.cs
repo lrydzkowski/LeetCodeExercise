b4df698b1a646d71141878667d62a28ad5fddca7
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-01-24 18:38:10 +0100

    feat: exercise 2101

---
+++ LeetCodeExercise.Core/Graphs/Exercise2101.cs
@@ -0,0 +1,89 @@
+﻿namespace LeetCodeExercise.Core.Graphs;
+
+public static class Exercise2101
+{
+    public static int MaximumDetonation(int[][] bombs)
+    {
+        if (bombs.Length == 0)
+        {
+            return 0;
+        }
+
+        List<List<int>> graph = BuildGraph(bombs);
+        int response = 0;
+        for (int i = 0; i < bombs.Length; i++)
+        {
+            Dictionary<int, bool> seen = new() { [i] = true };
+            response = Math.Max(response, TraverseGraph(i, graph, seen));
+        }
+
+        return response;
+    }
+
+    private static List<List<int>> BuildGraph(int[][] bombs)
+    {
+        List<List<int>> graph = new();
+        for (int i = 0; i < bombs.Length; i++)
+        {
+            graph.Add(new List<int>());
+            int[] currentBomb = bombs[i];
+            for (int j = 0; j < bombs.Length; j++)
+            {
+                if (i == j)
+                {
+                    continue;
+                }
+
+                int[] bombToCompare = bombs[j];
+                if (!CanTrigger(currentBomb, bombToCompare))
+                {
+                    continue;
+                }
+
+                graph[i].Add(j);
+            }
+        }
+
+        return graph;
+    }
+
+    private static bool CanTrigger(int[] leftBomb, int[] rightBomb)
+    {
+        long dx = Math.Abs(rightBomb[0] - leftBomb[0]);
+        long dy = Math.Abs(rightBomb[1] - leftBomb[1]);
+        long r = leftBomb[2];
+
+        if (dx + dy <= r)
+        {
+            return true;
+        }
+
+        if (dx > r || dy > r)
+        {
+            return false;
+        }
+
+        if (dx * dx + dy * dy <= r * r)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static int TraverseGraph(int node, List<List<int>> graph, Dictionary<int, bool> seen, int level = 0)
+    {
+        level++;
+        foreach (int nextNode in graph[node])
+        {
+            if (!seen.TryAdd(nextNode, true))
+            {
+                continue;
+            }
+
+            level = Math.Max(level, TraverseGraph(nextNode, graph, seen, level));
+        }
+
+        return level;
+    }
+}
---
+++ LeetCodeExercise.Tests/Graphs/Exercise2101Tests.cs
@@ -0,0 +1,84 @@
+﻿using LeetCodeExercise.Core.Graphs;
+
+namespace LeetCodeExercise.Tests.Graphs;
+
+public class Exercise2101Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int[][] bombs, int expectedResult)
+    {
+        int result = Exercise2101.MaximumDetonation(bombs);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            new[]
+            {
+                new[] { 2, 1, 3 },
+                new[] { 6, 1, 4 }
+            },
+            2
+        ];
+
+        yield return
+        [
+            new[]
+            {
+                new[] { 1, 1, 5 },
+                new[] { 10, 10, 5 }
+            },
+            1
+        ];
+
+        yield return
+        [
+            new[]
+            {
+                new[] { 1, 2, 3 },
+                new[] { 2, 3, 1 },
+                new[] { 3, 4, 2 },
+                new[] { 4, 5, 3 },
+                new[] { 5, 6, 4 }
+            },
+            5
+        ];
+
+        yield return
+        [
+            new[]
+            {
+                new[] { 1, 1, 100000 },
+                new[] { 100000, 100000, 1 }
+            },
+            1
+        ];
+
+        yield return
+        [
+            new[]
+            {
+                new[] { 7, 26, 7 },
+                new[] { 7, 18, 4 },
+                new[] { 3, 10, 7 },
+                new[] { 17, 50, 1 },
+                new[] { 3, 25, 10 },
+                new[] { 85, 23, 8 },
+                new[] { 80, 50, 1 },
+                new[] { 58, 74, 1 },
+                new[] { 38, 39, 7 },
+                new[] { 50, 51, 8 },
+                new[] { 31, 99, 3 },
+                new[] { 53, 6, 5 },
+                new[] { 59, 27, 10 },
+                new[] { 87, 78, 9 },
+                new[] { 68, 58, 3 }
+            },
+            3
+        ];
+    }
+}
