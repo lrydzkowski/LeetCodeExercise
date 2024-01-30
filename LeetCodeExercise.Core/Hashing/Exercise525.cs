948d4e02e68761428e466dd6a7aa1a651a9746e2
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-01-30 19:26:10 +0100

    feat: exercise 525

---
+++ LeetCodeExercise.Core/Hashing/Exercise525.cs
@@ -0,0 +1,78 @@
+﻿namespace LeetCodeExercise.Core.Hashing;
+
+public static class Exercise525
+{
+    public static int FindMaxLength(int[] nums)
+    {
+        if (nums.Length <= 1)
+        {
+            return 0;
+        }
+
+        int result = 0;
+        Dictionary<int, int> summary = new();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            summary[0] = 0;
+            summary[1] = 0;
+            for (int j = i; j < nums.Length; j++)
+            {
+                summary[nums[j]]++;
+                if (summary[0] == summary[1])
+                {
+                    result = Math.Max(result, j - i + 1);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static int FindMaxLength2(int[] nums)
+
+    {
+        int sum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+            nums[i] = sum;
+        }
+
+        int result = 0;
+        for (int i = nums.Length - 1; i > 0; i--)
+        {
+            int initialValue = nums[i];
+            int length = i + 1;
+            if (length < result)
+            {
+                continue;
+            }
+
+            if (length % 2 == 0 && initialValue == length / 2)
+            {
+                result = Math.Max(result, length);
+
+                continue;
+            }
+
+            for (int j = 1; j < i; j++)
+            {
+                int currentValue = initialValue - nums[j - 1];
+                length--;
+                if (length < result)
+                {
+                    break;
+                }
+
+                if (length % 2 == 0 && currentValue == length / 2)
+                {
+                    result = Math.Max(result, length);
+
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
---
+++ LeetCodeExercise.Tests/Hashing/Exercise525Tests.cs
@@ -0,0 +1,36 @@
+﻿using LeetCodeExercise.Core.Hashing;
+
+namespace LeetCodeExercise.Tests.Hashing;
+
+public class Exercise525Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int[] numbers, int expectedResult)
+    {
+        int result = Exercise525.FindMaxLength2(numbers);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            new[] { 0, 1 },
+            2
+        ];
+
+        yield return
+        [
+            new[] { 0, 1, 0 },
+            2
+        ];
+
+        yield return
+        [
+            new[] { 1, 1, 1, 1, 1, 1, 1, 1 },
+            0
+        ];
+    }
+}
