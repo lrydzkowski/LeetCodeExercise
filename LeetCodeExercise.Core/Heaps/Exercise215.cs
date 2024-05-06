e8caaa094b9e00b93efdab8c456c7f95393c613b
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-06 16:28:55 +0200

    feat: exercise 215

---
+++ LeetCodeExercise.Core/Heaps/Exercise215.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeExercise.Core.Heaps;
+
+public static class Exercise215
+{
+    public static int FindKthLargest(int[] numbers, int k)
+    {
+        PriorityQueue<int, int> priorityQueue = new();
+        foreach (int number in numbers)
+        {
+            priorityQueue.Enqueue(number * -1, number * -1);
+        }
+
+        int result = 0;
+        for (int i = 0; i < k; i++)
+        {
+            result = priorityQueue.Dequeue();
+        }
+
+        return result * -1;
+    }
+}
---
+++ LeetCodeExercise.Tests/Heaps/Exercise215Tests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCodeExercise.Core.Heaps;
+
+namespace LeetCodeExercise.Tests.Heaps;
+
+public class Exercise215Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int[] numbers, int k, int expectedResult)
+    {
+        int result = Exercise215.FindKthLargest(numbers, k);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            new[] { 3, 2, 1, 5, 6, 4 },
+            2,
+            5
+        ];
+    }
+}
