c3ced06e6a771d0bc89d5a10d8c04106274ed2e3
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-08 17:53:40 +0200

    feat: exercise 973

---
+++ LeetCodeExercise.Core/Heaps/Exercise973.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeExercise.Core.Heaps;
+
+public static class Exercise973
+{
+    public static int[][] Calculate(int[][] points, int k)
+    {
+        PriorityQueue<int[], double> priorityQueue = new();
+        foreach (int[] point in points)
+        {
+            int xDistance = point[0] - 0;
+            int yDistance = point[1] - 0;
+            double priority = Math.Sqrt((xDistance * xDistance) + (yDistance * yDistance));
+            priorityQueue.Enqueue(point, priority);
+        }
+
+        List<int[]> results = new();
+        for (int i = 0; i < k; i++)
+        {
+            results.Add(priorityQueue.Dequeue());
+        }
+
+        return results.ToArray();
+    }
+}
---
+++ LeetCodeExercise.Tests/Heaps/Exercise973Tests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCodeExercise.Core.Heaps;
+
+namespace LeetCodeExercise.Tests.Heaps;
+
+public class Exercise973Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int[][] points, int k, int[][] expectedResult)
+    {
+        int[][] result = Exercise973.Calculate(points, k);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            new[] { new[] { 3, 3 }, new[] { 5, -1 }, new[] { -2, 4 } },
+            2,
+            new[] { new[] { 3, 3 }, new[] { -2, 4 } }
+        ];
+    }
+}
