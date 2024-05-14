b7f00013e72fe42330bfb1b0f877448ecf85c772
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-14 20:33:19 +0200

    feat: exercise 1710

---
+++ LeetCodeExercise.Core/Greedy/Exercise1710.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeExercise.Core.Greedy;
+
+public static class Exercise1710
+{
+    public static int MaximumUnits(int[][] boxTypes, int truckSize)
+    {
+        boxTypes = boxTypes.OrderByDescending(x => x[1]).ToArray();
+        int response = 0;
+        int index = 0;
+        for (int i = 0; i < truckSize; i++)
+        {
+            response += boxTypes[index][1];
+            boxTypes[index][0]--;
+            if (boxTypes[index][0] == 0)
+            {
+                index++;
+                if (index == boxTypes.Length)
+                {
+                    break;
+                }
+            }
+        }
+
+        return response;
+    }
+}
---
+++ LeetCodeExercise.Tests/Greedy/Exercise1710Tests.cs
@@ -0,0 +1,43 @@
+﻿using LeetCodeExercise.Core.Greedy;
+
+namespace LeetCodeExercise.Tests.Greedy;
+
+public class Exercise1710Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int[][] boxTypes, int trucSize, int expectedResult)
+    {
+        int result = Exercise1710.MaximumUnits(boxTypes, trucSize);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            new[] { new[] { 1, 3 }, new[] { 2, 2 }, new[] { 3, 1 } },
+            4,
+            8
+        ];
+
+        yield return
+        [
+            new[] { new[] { 5, 10 }, new[] { 2, 5 }, new[] { 4, 7 }, new[] { 3, 9 } },
+            10,
+            91
+        ];
+
+        yield return
+        [
+            new[]
+            {
+                new[] { 1, 3 }, new[] { 5, 5 }, new[] { 2, 5 }, new[] { 4, 2 }, new[] { 4, 1 }, new[] { 3, 1 },
+                new[] { 2, 2 }, new[] { 1, 3 }, new[] { 2, 5 }, new[] { 3, 2 }
+            },
+            35,
+            76
+        ];
+    }
+}
