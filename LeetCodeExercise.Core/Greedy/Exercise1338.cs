a3920f9a2619c172c569e77c4b536975bbe9ee61
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-15 17:30:12 +0200

    feat: exercise 1338

---
+++ LeetCodeExercise.Core/Greedy/Exercise1338.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeExercise.Core.Greedy;
+
+public static class Exercise1338
+{
+    public static int MinSetSize(int[] arr)
+    {
+        Dictionary<int, int> occurrences = new();
+        foreach (int val in arr)
+        {
+            occurrences.TryAdd(val, 0);
+            occurrences[val]++;
+        }
+
+        int operations = 0;
+        int currentLength = arr.Length;
+        int halfLength = currentLength / 2;
+        List<int[]> elements =
+            occurrences.Select(x => new[] { x.Key, x.Value }).OrderByDescending(x => x[1]).ToList();
+        foreach (int[] element in elements)
+        {
+            currentLength -= element[1];
+            operations++;
+            if (currentLength <= halfLength)
+            {
+                break;
+            }
+        }
+
+        return operations;
+    }
+}
---
+++ LeetCodeExercise.Tests/Greedy/Exercise1338Tests.cs
@@ -0,0 +1,30 @@
+﻿using LeetCodeExercise.Core.Greedy;
+
+namespace LeetCodeExercise.Tests.Greedy;
+
+public class Exercise1338Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int[] arr, int expectedResult)
+    {
+        int result = Exercise1338.MinSetSize(arr);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            new[] { 3, 3, 3, 3, 5, 5, 5, 2, 2, 7 },
+            2
+        ];
+
+        yield return
+        [
+            new[] { 7, 7, 7, 7, 7, 7 },
+            1
+        ];
+    }
+}
