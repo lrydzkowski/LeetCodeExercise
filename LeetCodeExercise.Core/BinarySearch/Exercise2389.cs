337141363e417662ba8b11a2837fa7e05a0478e9
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-16 18:55:01 +0200

    feat: exercise 2389

---
+++ LeetCodeExercise.Core/BinarySearch/Exercise2389.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCodeExercise.Core.BinarySearch;
+
+public static class Exercise2389
+{
+    public static int[] AnswerQueries(int[] nums, int[] queries)
+    {
+        nums = nums.OrderBy(x => x).ToArray();
+        for (int i = 1; i < nums.Length; i++)
+        {
+            nums[i] += nums[i - 1];
+        }
+
+        List<int> answers = new();
+        foreach (int query in queries)
+        {
+            int currentAnswer = -1;
+
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = (int)Math.Floor((left + right) / 2m);
+                if (nums[mid] == query)
+                {
+                    currentAnswer = mid + 1;
+
+                    break;
+                }
+
+                if (nums[mid] > query)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            if (currentAnswer == -1)
+            {
+                currentAnswer = left;
+            }
+
+            answers.Add(currentAnswer);
+        }
+
+        return answers.ToArray();
+    }
+}
---
+++ LeetCodeExercise.Tests/BinarySearch/Exercise2389Tests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCodeExercise.Core.BinarySearch;
+
+namespace LeetCodeExercise.Tests.BinarySearch;
+
+public class Exercise2389Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int[] nums, int[] queries, int[] expectedResult)
+    {
+        int[] result = Exercise2389.AnswerQueries(nums, queries);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            new[] { 4, 5, 2, 1 },
+            new[] { 3, 10, 21 },
+            new[] { 2, 3, 4 }
+        ];
+    }
+}
