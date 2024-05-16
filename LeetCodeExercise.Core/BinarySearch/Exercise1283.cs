30b084f53697b1e8a09dd4670a2267ba26b6d67f
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-16 19:24:14 +0200

    feat: exercise 1283

---
+++ LeetCodeExercise.Core/BinarySearch/Exercise1283.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCodeExercise.Core.BinarySearch;
+
+public static class Exercise1283
+{
+    public static int SmallestDivisor(int[] nums, int threshold)
+    {
+        int left = 1;
+        int right = nums.Max();
+        while (left <= right)
+        {
+            int mid = (int)Math.Floor((left + right) / 2m);
+            if (Check(nums, threshold, mid))
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private static bool Check(int[] nums, int threshold, int divider)
+    {
+        int sum = 0;
+        foreach (int num in nums)
+        {
+            sum += (int)Math.Ceiling((decimal)num / divider);
+            if (sum > threshold)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ LeetCodeExercise.Tests/BinarySearch/Exercise1283Tests.cs
@@ -0,0 +1,32 @@
+﻿using LeetCodeExercise.Core.BinarySearch;
+
+namespace LeetCodeExercise.Tests.BinarySearch;
+
+public class Exercise1283Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int[] nums, int threshold, int expectedResult)
+    {
+        int result = Exercise1283.SmallestDivisor(nums, threshold);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            new[] { 1, 2, 5, 9 },
+            6,
+            5
+        ];
+
+        yield return
+        [
+            new[] { 44, 22, 33, 11, 1 },
+            5,
+            44
+        ];
+    }
+}
