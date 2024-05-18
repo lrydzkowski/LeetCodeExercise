46a2ecbe01339d86294999f51367e3f1f016cf06
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-18 18:49:41 +0200

    feat: exercise 1231

---
+++ LeetCodeExercise.Core/BinarySearch/Exercise1231.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCodeExercise.Core.BinarySearch;
+
+public static class Exercise1231
+{
+    public static int MaximizeSweetness(int[] sweetness, int k)
+    {
+        if (k == 0)
+        {
+            return sweetness.Sum();
+        }
+
+        int left = 1;
+        int right = (int)Math.Ceiling((decimal)sweetness.Sum() / k);
+        while (left <= right)
+        {
+            int mid = (int)Math.Floor((left + right) / 2m);
+            if (FulfillRequirements(sweetness, k, mid))
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return right;
+    }
+
+    private static bool FulfillRequirements(int[] sweetness, int k, int value)
+    {
+        int currentSum = 0;
+        int parts = 0;
+        foreach (int currentValue in sweetness)
+        {
+            currentSum += currentValue;
+            if (currentSum >= value)
+            {
+                currentSum = 0;
+                parts++;
+            }
+        }
+
+        if (parts >= k + 1)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ LeetCodeExercise.Tests/BinarySearch/Exercise1231Tests.cs
@@ -0,0 +1,39 @@
+﻿using LeetCodeExercise.Core.BinarySearch;
+
+namespace LeetCodeExercise.Tests.BinarySearch;
+
+public class Exercise1231Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int[] sweetness, int k, int expectedResult)
+    {
+        int result = Exercise1231.MaximizeSweetness(sweetness, k);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+            5,
+            6
+        ];
+
+        yield return
+        [
+            new[] { 5, 6, 7, 8, 9, 1, 2, 3, 4 },
+            8,
+            1
+        ];
+
+        yield return
+        [
+            new[] { 1, 2, 2, 1, 2, 2, 1, 2, 2 },
+            2,
+            5
+        ];
+    }
+}
