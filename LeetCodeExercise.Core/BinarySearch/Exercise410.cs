e7cc849ea2215d3e06bdbdad19964ad6e9b6fb8c
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-19 08:32:04 +0200

    feat: exercise 410

---
+++ LeetCodeExercise.Core/BinarySearch/Exercise410.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCodeExercise.Core.BinarySearch;
+
+public static class Exercise410
+{
+    public static int SplitArray(int[] nums, int k)
+    {
+        if (k == 1)
+        {
+            return nums.Sum();
+        }
+
+        int left = 0;
+        int right = nums.Sum();
+        while (left <= right)
+        {
+            int mid = (int)Math.Floor((left + right) / 2m);
+            if (CanFindAnswer(nums, k, mid))
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private static bool CanFindAnswer(int[] nums, int k, int value)
+    {
+        if (nums.Length == 0)
+        {
+            return false;
+        }
+
+        int currentSum = nums[0];
+        int currentLength = k - 1;
+        List<int> sums = [];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            int num = nums[i];
+            if (currentSum + num > value || nums.Length - i == currentLength)
+            {
+                sums.Add(currentSum);
+                currentSum = 0;
+                currentLength--;
+            }
+
+            currentSum += num;
+        }
+
+        sums.Add(currentSum);
+
+        return sums.Count == k && sums.Max() <= value && sums.All(x => x <= value);
+    }
+}
---
+++ LeetCodeExercise.Tests/BinarySearch/Exercise410Tests.cs
@@ -0,0 +1,60 @@
+﻿using LeetCodeExercise.Core.BinarySearch;
+
+namespace LeetCodeExercise.Tests.BinarySearch;
+
+public class Exercise410Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int[] nums, int k, int expectedResult)
+    {
+        int result = Exercise410.SplitArray(nums, k);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            new[] { 7, 2, 5, 10, 8 },
+            2,
+            18
+        ];
+
+        yield return
+        [
+            new[] { 1, 2, 3, 4, 5 },
+            2,
+            9
+        ];
+
+        yield return
+        [
+            new[] { 1, 2, 3, 4, 5 },
+            1,
+            15
+        ];
+
+        yield return
+        [
+            new[] { 10, 5, 13, 4, 8, 4, 5, 11, 14, 9, 16, 10, 20, 8 },
+            8,
+            25
+        ];
+
+        yield return
+        [
+            new[] { 1, 4, 4 },
+            3,
+            4
+        ];
+
+        yield return
+        [
+            new[] { 5, 2, 4, 1, 3, 6, 0 },
+            4,
+            6
+        ];
+    }
+}
