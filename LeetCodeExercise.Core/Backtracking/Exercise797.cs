ca20263d520bc85a50c4831fe45f966bdbfc0abe
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-19 15:33:10 +0200

    feat: exercise 797

---
+++ LeetCodeExercise.Core/Backtracking/Exercise797.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeExercise.Core.Backtracking;
+
+public static class Exercise797
+{
+    public static IList<IList<int>> AllPathsSourceTarget(int[][] graph)
+    {
+        List<List<int>> answers = GoThroughGraph(graph, 0, [0], new List<List<int>>());
+
+        return answers.Select(x => x).Cast<IList<int>>().ToList();
+    }
+
+    private static List<List<int>> GoThroughGraph(int[][] graph, int index, List<int> path, List<List<int>> answers)
+    {
+        if (index == graph.Length - 1)
+        {
+            answers.Add(path.Select(x => x).ToList());
+
+            return answers;
+        }
+
+        foreach (int nextIndex in graph[index])
+        {
+            path.Add(nextIndex);
+            answers = GoThroughGraph(graph, nextIndex, path, answers);
+            path.RemoveAt(path.Count - 1);
+        }
+
+        return answers;
+    }
+}
---
+++ LeetCodeExercise.Tests/Backtracking/Exercise797Tests.cs
@@ -0,0 +1,41 @@
+﻿using LeetCodeExercise.Core.Backtracking;
+
+namespace LeetCodeExercise.Tests.Backtracking;
+
+public class Exercise797Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int[][] graph, List<List<int>> expectedResult)
+    {
+        IList<IList<int>> result = Exercise797.AllPathsSourceTarget(graph);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            new[] { new[] { 1, 2 }, new[] { 3 }, new[] { 3 }, [] },
+            new List<List<int>>
+            {
+                new() { 0, 1, 3 },
+                new() { 0, 2, 3 }
+            }
+        ];
+
+        yield return
+        [
+            new[] { new[] { 4, 3, 1 }, new[] { 3, 2, 4 }, new[] { 3 }, new[] { 4 }, [] },
+            new List<List<int>>
+            {
+                new() { 0, 4 },
+                new() { 0, 3, 4 },
+                new() { 0, 1, 3, 4 },
+                new() { 0, 1, 2, 3, 4 },
+                new() { 0, 1, 4 }
+            }
+        ];
+    }
+}
