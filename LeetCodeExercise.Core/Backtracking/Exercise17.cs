0050f155eb75c87d37865dac31d50e05fc302906
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-19 16:35:44 +0200

    feat: exercise 17

---
+++ LeetCodeExercise.Core/Backtracking/Exercise17.cs
@@ -0,0 +1,64 @@
+﻿namespace LeetCodeExercise.Core.Backtracking;
+
+public static class Exercise17
+{
+    public static IList<string> LetterCombinations(string digits)
+    {
+        if (digits.Length == 0)
+        {
+            return new List<string>();
+        }
+
+        Dictionary<int, List<char>> dict = new()
+        {
+            [2] = ['a', 'b', 'c'],
+            [3] = ['d', 'e', 'f'],
+            [4] = ['g', 'h', 'i'],
+            [5] = ['j', 'k', 'l'],
+            [6] = ['m', 'n', 'o'],
+            [7] = ['p', 'q', 'r', 's'],
+            [8] = ['t', 'u', 'v'],
+            [9] = ['w', 'x', 'y', 'z']
+        };
+        List<string> answers = GoThroughGraph(
+            dict,
+            digits.Select(ToInt).ToArray(),
+            0,
+            new List<char>(),
+            new List<string>()
+        );
+
+        return answers;
+    }
+
+    private static List<string> GoThroughGraph(
+        Dictionary<int, List<char>> dict,
+        int[] digits,
+        int index,
+        List<char> path,
+        List<string> answers
+    )
+    {
+        if (digits.Length == index)
+        {
+            answers.Add(string.Join("", path));
+
+            return answers;
+        }
+
+        int digit = digits[index];
+        foreach (char character in dict[digit])
+        {
+            path.Add(character);
+            answers = GoThroughGraph(dict, digits, index + 1, path, answers);
+            path.RemoveAt(path.Count - 1);
+        }
+
+        return answers;
+    }
+
+    private static int ToInt(this char c)
+    {
+        return (int)(c - '0');
+    }
+}
---
+++ LeetCodeExercise.Tests/Backtracking/Exercise17Tests.cs
@@ -0,0 +1,36 @@
+﻿using LeetCodeExercise.Core.Backtracking;
+
+namespace LeetCodeExercise.Tests.Backtracking;
+
+public class Exercise17Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(string digits, IList<string> expectedResult)
+    {
+        IList<string> result = Exercise17.LetterCombinations(digits);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            "23",
+            new List<string> { "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" }
+        ];
+
+        yield return
+        [
+            "",
+            new List<string>()
+        ];
+
+        yield return
+        [
+            "2",
+            new List<string>() { "a", "b", "c" }
+        ];
+    }
+}
