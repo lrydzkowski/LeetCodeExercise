482cfa6da6108c534419cbc6f2fa6868f85faf28
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-20 18:27:18 +0200

    feat: exercise 967

---
+++ LeetCodeExercise.Core/Backtracking/Exercise967.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCodeExercise.Core.Backtracking;
+
+public static class Exercise967
+{
+    public static int[] NumsSameConsecDiff(int n, int k)
+    {
+        List<string> results = FindAnswers(n, k, new List<int>(), new List<string>());
+
+        return results.Select(x => Convert.ToInt32(x)).ToArray();
+    }
+
+    private static List<string> FindAnswers(int n, int k, List<int> path, List<string> results)
+    {
+        if (path.Count == n)
+        {
+            results.Add(string.Join("", path));
+
+            return results;
+        }
+
+        if (path.Count == 0)
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                path.Add(i);
+                results = FindAnswers(n, k, path, results);
+                path.RemoveAt(path.Count - 1);
+            }
+
+            return results;
+        }
+
+        int current = path[^1];
+        if (current - k >= 0)
+        {
+            path.Add(current - k);
+            results = FindAnswers(n, k, path, results);
+            path.RemoveAt(path.Count - 1);
+        }
+
+        if (k > 0 && current + k <= 9)
+        {
+            path.Add(current + k);
+            results = FindAnswers(n, k, path, results);
+            path.RemoveAt(path.Count - 1);
+        }
+
+        return results;
+    }
+}
---
+++ LeetCodeExercise.Tests/Backtracking/Exercise967Tests.cs
@@ -0,0 +1,39 @@
+﻿using LeetCodeExercise.Core.Backtracking;
+
+namespace LeetCodeExercise.Tests.Backtracking;
+
+public class Exercise967Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int n, int k, int[] expectedResult)
+    {
+        int[] result = Exercise967.NumsSameConsecDiff(n, k);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            3,
+            7,
+            new int[] { 181, 292, 707, 818, 929 }
+        ];
+
+        yield return
+        [
+            2,
+            1,
+            new int[] { 10, 12, 21, 23, 32, 34, 43, 45, 54, 56, 65, 67, 76, 78, 87, 89, 98 }
+        ];
+
+        yield return
+        [
+            2,
+            0,
+            new int[] { 11, 22, 33, 44, 55, 66, 77, 88, 99 }
+        ];
+    }
+}
