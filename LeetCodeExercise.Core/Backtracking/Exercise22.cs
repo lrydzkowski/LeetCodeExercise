39c885fa45577b6a6f0c80e16561e35acc1dc792
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-20 17:37:48 +0200

    feat: exercise 22

---
+++ LeetCodeExercise.Core/Backtracking/Exercise22.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCodeExercise.Core.Backtracking;
+
+public static class Exercise22
+{
+    public static IList<string> GenerateParenthesis(int n)
+    {
+        IList<string> results = FindAnswers(n, n, new List<char>(), new List<string>());
+
+        return results;
+    }
+
+    private static IList<string> FindAnswers(int openings, int closures, List<char> path, IList<string> results)
+    {
+        if (openings > 0)
+        {
+            path.Add('(');
+            results = FindAnswers(openings - 1, closures, path, results);
+            path.RemoveAt(path.Count - 1);
+        }
+
+        if (closures > openings)
+        {
+            path.Add(')');
+            results = FindAnswers(openings, closures - 1, path, results);
+            path.RemoveAt(path.Count - 1);
+        }
+
+        if (openings == 0 && closures == 0)
+        {
+            results.Add(string.Join("", path));
+        }
+
+        return results;
+    }
+}
---
+++ LeetCodeExercise.Tests/Backtracking/Exercise22Tests.cs
@@ -0,0 +1,30 @@
+﻿using LeetCodeExercise.Core.Backtracking;
+
+namespace LeetCodeExercise.Tests.Backtracking;
+
+public class Exercise22Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int n, IList<string> expectedResult)
+    {
+        IList<string> result = Exercise22.GenerateParenthesis(n);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            3,
+            new List<string> { "((()))", "(()())", "(())()", "()(())", "()()()" }
+        ];
+
+        yield return
+        [
+            1,
+            new List<string> { "()" }
+        ];
+    }
+}
