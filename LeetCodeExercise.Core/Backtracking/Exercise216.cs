b5098f9e841b280d95a07f9d983a64df8a0408f8
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-20 19:05:54 +0200

    feat: exercise 216

---
+++ LeetCodeExercise.Core/Backtracking/Exercise216.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCodeExercise.Core.Backtracking;
+
+public static class Exercise216
+{
+    public static IList<IList<int>> CombinationSum3(int k, int n)
+    {
+        List<List<int>> results = FindResults(k, n, 0, new List<int>(), new List<List<int>>());
+
+        return results.Select(x => new HashSet<int>(x))
+            .Distinct(HashSet<int>.CreateSetComparer())
+            .Select(x => (IList<int>)x.ToList())
+            .ToList();
+    }
+
+    private static List<List<int>> FindResults(int k, int n, int currentSum, List<int> path, List<List<int>> results)
+    {
+        if (path.Count == k && currentSum == n)
+        {
+            results.Add(path.Select(x => x).ToList());
+
+            return results;
+        }
+
+        for (int i = 1; i <= 9; i++)
+        {
+            if (currentSum + i > n)
+            {
+                break;
+            }
+
+            if (path.Contains(i))
+            {
+                continue;
+            }
+
+            path.Add(i);
+            results = FindResults(k, n, currentSum + i, path, results);
+            path.RemoveAt(path.Count - 1);
+        }
+
+        return results;
+    }
+}
---
+++ LeetCodeExercise.Tests/Backtracking/Exercise216Tests.cs
@@ -0,0 +1,47 @@
+﻿using LeetCodeExercise.Core.Backtracking;
+
+namespace LeetCodeExercise.Tests.Backtracking;
+
+public class Exercise216Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int k, int n, List<List<int>> expectedResult)
+    {
+        IList<IList<int>> result = Exercise216.CombinationSum3(k, n);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            3,
+            7,
+            new List<List<int>>
+            {
+                new() { 1, 2, 4 }
+            }
+        ];
+
+        yield return
+        [
+            3,
+            9,
+            new List<List<int>>
+            {
+                new() { 1, 2, 6 },
+                new() { 1, 3, 5 },
+                new() { 2, 3, 4 }
+            }
+        ];
+
+        yield return
+        [
+            4,
+            1,
+            new List<List<int>>()
+        ];
+    }
+}
