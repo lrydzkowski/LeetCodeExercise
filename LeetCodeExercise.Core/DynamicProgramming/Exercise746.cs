83baeb050fd15c87365d8846ab0b2d306981c8a2
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-24 18:58:00 +0200

    feat: exercise 746

---
+++ LeetCodeExercise.Core/DynamicProgramming/Exercise746.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeExercise.Core.DynamicProgramming;
+
+public static class Exercise746
+{
+    public static int MinCostClimbingStairs(int[] cost)
+    {
+        if (cost.Length == 0)
+        {
+            return 0;
+        }
+
+        return Calculate(cost, cost.Length, new Dictionary<int, int>());
+    }
+
+    private static int Calculate(int[] cost, int i, Dictionary<int, int> cache)
+    {
+        if (i <= 1)
+        {
+            return cost[i];
+        }
+
+        if (cache.ContainsKey(i))
+        {
+            return cache[i];
+        }
+
+        int currentCost = i == cost.Length ? 0 : cost[i];
+        currentCost = Math.Min(
+            Calculate(cost, i - 1, cache) + currentCost,
+            Calculate(cost, i - 2, cache) + currentCost
+        );
+        cache[i] = currentCost;
+
+        return currentCost;
+    }
+}
---
+++ LeetCodeExercise.Tests/DynamicProgramming/Exercise746Tests.cs
@@ -0,0 +1,30 @@
+﻿using LeetCodeExercise.Core.DynamicProgramming;
+
+namespace LeetCodeExercise.Tests.DynamicProgramming;
+
+public class Exercise746Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int[] cost, int expectedResult)
+    {
+        int result = Exercise746.MinCostClimbingStairs(cost);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            new[] { 10, 15, 20 },
+            15
+        ];
+
+        yield return
+        [
+            new[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 },
+            6
+        ];
+    }
+}
