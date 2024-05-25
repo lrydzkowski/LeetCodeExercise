3bba2e2375d3cd14642ee426d2871acf8af8d746
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-25 18:21:44 +0200

    feat: exercise 714

---
+++ LeetCodeExercise.Core/DynamicProgramming/Exercise714.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCodeExercise.Core.DynamicProgramming;
+
+public static class Exercise714
+{
+    public static int MaxProfit(int[] prices, int fee)
+    {
+        return FindAnswer(prices, fee, false, 0, new Dictionary<int, Dictionary<int, int>>());
+    }
+
+    private static int FindAnswer(
+        int[] prices,
+        int fee,
+        bool owns,
+        int index,
+        Dictionary<int, Dictionary<int, int>> cache
+    )
+    {
+        if (index == prices.Length)
+        {
+            return 0;
+        }
+
+        if (!cache.ContainsKey(index))
+        {
+            cache[index] = new Dictionary<int, int>();
+        }
+
+        if (cache[index].ContainsKey(owns ? 1 : 0))
+        {
+            return cache[index][owns ? 1 : 0];
+        }
+
+        int ans = FindAnswer(prices, fee, owns, index + 1, cache);
+        ans = owns
+            ? Math.Max(ans, FindAnswer(prices, fee, false, index + 1, cache) + prices[index])
+            : Math.Max(ans, FindAnswer(prices, fee, true, index + 1, cache) - fee - prices[index]);
+        cache[index][owns ? 1 : 0] = ans;
+
+        return ans;
+    }
+}
---
+++ LeetCodeExercise.Tests/DynamicProgramming/Exercise714Tests.cs
@@ -0,0 +1,32 @@
+﻿using LeetCodeExercise.Core.DynamicProgramming;
+
+namespace LeetCodeExercise.Tests.DynamicProgramming;
+
+public class Exercise714Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int[] prices, int fee, int expectedResult)
+    {
+        int result = Exercise714.MaxProfit(prices, fee);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            new[] { 1, 3, 2, 8, 4, 9 },
+            2,
+            8
+        ];
+
+        yield return
+        [
+            new[] { 1, 3, 7, 5, 10, 3 },
+            3,
+            6
+        ];
+    }
+}
