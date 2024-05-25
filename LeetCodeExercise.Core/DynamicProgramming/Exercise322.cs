c7d7a85a72764982ca97c3479a62300ea0754491
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-25 08:33:54 +0200

    feat: exercise 322

---
+++ LeetCodeExercise.Core/DynamicProgramming/Exercise322.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCodeExercise.Core.DynamicProgramming;
+
+public static class Exercise322
+{
+    public static int CoinChange(int[] coins, int amount)
+    {
+        if (amount == 0)
+        {
+            return 0;
+        }
+
+        return FindAnswer(coins, amount, new Dictionary<int, int>());
+    }
+
+    private static int FindAnswer(
+        int[] coins,
+        int amount,
+        Dictionary<int, int> cache
+    )
+    {
+        int min = int.MaxValue;
+        foreach (int coin in coins)
+        {
+            if (amount - coin == 0)
+            {
+                min = 1;
+
+                continue;
+            }
+
+            if (amount - coin < 0)
+            {
+                continue;
+            }
+
+            int currentOperations = cache.ContainsKey(amount - coin)
+                ? cache[amount - coin]
+                : FindAnswer(coins, amount - coin, cache);
+            if (currentOperations == -1)
+            {
+                continue;
+            }
+
+            currentOperations += 1;
+            min = Math.Min(min, currentOperations);
+        }
+
+        int answer = min == int.MaxValue ? -1 : min;
+        cache[amount] = answer;
+
+        return answer;
+    }
+}
---
+++ LeetCodeExercise.Tests/DynamicProgramming/Exercise322Tests.cs
@@ -0,0 +1,39 @@
+﻿using LeetCodeExercise.Core.DynamicProgramming;
+
+namespace LeetCodeExercise.Tests.DynamicProgramming;
+
+public class Exercise322Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int[] coins, int amount, int expectedResult)
+    {
+        int result = Exercise322.CoinChange(coins, amount);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            new[] { 1, 2, 5 },
+            11,
+            3
+        ];
+
+        yield return
+        [
+            new[] { 2 },
+            3,
+            -1
+        ];
+
+        yield return
+        [
+            new[] { 1 },
+            0,
+            0
+        ];
+    }
+}
