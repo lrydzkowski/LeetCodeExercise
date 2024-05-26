4468a79e8504ded4a695259d6ba37a795cfc9183
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-26 08:47:05 +0200

    feat: exercise 63

---
+++ LeetCodeExercise.Core/DynamicProgramming/Exercise63.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCodeExercise.Core.DynamicProgramming;
+
+public static class Exercise63
+{
+    public static int UniquePathsWithObstacles(int[][] obstacleGrid)
+    {
+        return FindAnswer(
+            obstacleGrid,
+            obstacleGrid.Length - 1,
+            obstacleGrid[^1].Length - 1,
+            new Dictionary<string, int>()
+        );
+    }
+
+    private static int FindAnswer(int[][] obstacleGrid, int x, int y, Dictionary<string, int> cache)
+    {
+        if (obstacleGrid[x][y] == 1)
+        {
+            return 0;
+        }
+
+        if (x + y == 0)
+        {
+            return 1;
+        }
+
+        string cacheKey = $"{x}-{y}";
+        if (cache.TryGetValue(cacheKey, out int cachedPath))
+        {
+            return cachedPath;
+        }
+
+        int paths = x > 0 ? FindAnswer(obstacleGrid, x - 1, y, cache) : 0;
+        paths += y > 0 ? FindAnswer(obstacleGrid, x, y - 1, cache) : 0;
+        cache[cacheKey] = paths;
+
+        return paths;
+    }
+}
---
+++ LeetCodeExercise.Tests/DynamicProgramming/Exercise63Tests.cs
@@ -0,0 +1,30 @@
+﻿using LeetCodeExercise.Core.DynamicProgramming;
+
+namespace LeetCodeExercise.Tests.DynamicProgramming;
+
+public class Exercise63Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int[][] obstacleGrid, int expectedResult)
+    {
+        int result = Exercise63.UniquePathsWithObstacles(obstacleGrid);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            new[] { new[] { 0, 0, 0 }, new[] { 0, 1, 0 }, new[] { 0, 0, 0 } },
+            2
+        ];
+
+        yield return
+        [
+            new[] { new[] { 0, 1 }, new[] { 0, 0 } },
+            1
+        ];
+    }
+}
