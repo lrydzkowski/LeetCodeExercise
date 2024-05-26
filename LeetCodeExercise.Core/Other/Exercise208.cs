2cbf63417133a48ac9d96bf232d9d37d16acddea
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-26 18:15:27 +0200

    feat: exercise 208

---
+++ LeetCodeExercise.Core/Other/Exercise208.cs
@@ -0,0 +1,72 @@
+﻿namespace LeetCodeExercise.Core.Other;
+
+public class Exercise208
+{
+    private readonly TieNode _root = new('_');
+
+    public void Insert(string word)
+    {
+        TieNode current = _root;
+        foreach (char c in word)
+        {
+            TieNode? children = current.Children.FirstOrDefault(x => x.Value == c);
+            if (children == null)
+            {
+                children = new TieNode(c);
+                current.Children.Add(children);
+            }
+
+            current = children;
+        }
+
+        current.Words.Add(word);
+    }
+
+    public bool Search(string word)
+    {
+        TieNode current = _root;
+        foreach (char c in word)
+        {
+            TieNode? children = current.Children.FirstOrDefault(x => x.Value == c);
+            if (children == null)
+            {
+                return false;
+            }
+
+            current = children;
+        }
+
+        return current.Words.Contains(word);
+    }
+
+    public bool StartsWith(string prefix)
+    {
+        TieNode current = _root;
+        foreach (char c in prefix)
+        {
+            TieNode? children = current.Children.FirstOrDefault(x => x.Value == c);
+            if (children == null)
+            {
+                return false;
+            }
+
+            current = children;
+        }
+
+        return true;
+    }
+
+    private class TieNode
+    {
+        public char Value { get; set; }
+
+        public List<TieNode> Children { get; set; } = [];
+
+        public List<string> Words { get; set; } = [];
+
+        public TieNode(char value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ LeetCodeExercise.Tests/Other/Exercise208Tests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCodeExercise.Core.Other;
+
+namespace LeetCodeExercise.Tests.Other;
+
+public class Exercise208Tests
+{
+    [Fact]
+    public void Test()
+    {
+        List<bool> expectedResults = [true, false, true, true];
+        List<bool> results = new();
+
+        Exercise208 exercise208 = new();
+        exercise208.Insert("apple");
+        results.Add(exercise208.Search("apple"));
+        results.Add(exercise208.Search("app"));
+        results.Add(exercise208.StartsWith("app"));
+        exercise208.Insert("app");
+        results.Add(exercise208.Search("app"));
+
+        Assert.Equal(expectedResults, results);
+    }
+}
