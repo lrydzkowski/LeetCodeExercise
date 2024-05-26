c936fa07f0b7c040ee35d23e29c8f75dc605ffc9
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-26 16:50:21 +0200

    feat: exercise 931

---
+++ LeetCodeExercise.Core/DynamicProgramming/Exercise931.cs
@@ -0,0 +1,56 @@
+﻿namespace LeetCodeExercise.Core.DynamicProgramming;
+
+public static class Exercise931
+{
+    public static int MinFallingPathSum(int[][] matrix)
+    {
+        if (matrix.Length == 0)
+        {
+            return 0;
+        }
+
+        Dictionary<string, int> cache = new();
+        List<int> possibleValues = new();
+        int row = matrix.Length - 1;
+        for (int i = 0; i < matrix[row].Length; i++)
+        {
+            possibleValues.Add(FindAnswer(matrix, row, i, cache));
+        }
+
+        return possibleValues.Min();
+    }
+
+    private static int FindAnswer(int[][] matrix, int x, int y, Dictionary<string, int> cache)
+    {
+        if (y < 0 || y > matrix.Length - 1)
+        {
+            return int.MaxValue;
+        }
+
+        if (x == 0)
+        {
+            return matrix[0][y];
+        }
+
+        string cacheKey = $"{x}-{y}";
+        if (cache.TryGetValue(cacheKey, out int cachedAnswer))
+        {
+            return cachedAnswer;
+        }
+
+        List<int> possiblePreviousValues = new();
+        for (int i = -1; i <= 1; i++)
+        {
+            string possiblePreviousValueCacheKey = $"{x - 1}-{y + i}";
+            int possiblePreviousValue = cache.TryGetValue(possiblePreviousValueCacheKey, out int value)
+                ? value
+                : FindAnswer(matrix, x - 1, y + i, cache);
+            possiblePreviousValues.Add(possiblePreviousValue);
+        }
+
+        int answer = possiblePreviousValues.Min() + matrix[x][y];
+        cache[cacheKey] = answer;
+
+        return answer;
+    }
+}
---
+++ LeetCodeExercise.Tests/DynamicProgramming/Exercise931Tests.cs
@@ -0,0 +1,30 @@
+﻿using LeetCodeExercise.Core.DynamicProgramming;
+
+namespace LeetCodeExercise.Tests.DynamicProgramming;
+
+public class Exercise931Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int[][] matrix, int expectedResult)
+    {
+        int result = Exercise931.MinFallingPathSum(matrix);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            new[] { new[] { 2, 1, 3 }, new[] { 6, 5, 4 }, new[] { 7, 8, 9 } },
+            13
+        ];
+
+        yield return
+        [
+            new[] { new[] { -19, 57 }, new[] { -40, -5 } },
+            -59
+        ];
+    }
+}
