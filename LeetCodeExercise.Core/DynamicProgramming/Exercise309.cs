9be9a7e2211cef817c8cddc4594a2f1e324fd3a3
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-26 07:52:11 +0200

    feat: exercise 309

---
+++ LeetCodeExercise.Core/DynamicProgramming/Exercise309.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCodeExercise.Core.DynamicProgramming;
+
+public static class Exercise309
+{
+    public static int MaxProfit(int[] prices)
+    {
+        return FindAnswer(prices, false, false, 0, new Dictionary<int, Dictionary<string, int>>());
+    }
+
+    private static int FindAnswer(
+        int[] prices,
+        bool owns,
+        bool coolDown,
+        int index,
+        Dictionary<int, Dictionary<string, int>> cache
+    )
+    {
+        if (index == prices.Length)
+        {
+            return 0;
+        }
+
+        if (!cache.ContainsKey(index))
+        {
+            cache[index] = new Dictionary<string, int>();
+        }
+
+        string cacheKey = $"{owns}-{coolDown}";
+        if (cache[index].ContainsKey(cacheKey))
+        {
+            return cache[index][cacheKey];
+        }
+
+        int ans = FindAnswer(prices, owns, false, index + 1, cache);
+        if (!coolDown)
+        {
+            ans = owns
+                ? Math.Max(ans, FindAnswer(prices, false, true, index + 1, cache) + prices[index])
+                : Math.Max(ans, FindAnswer(prices, true, false, index + 1, cache) - prices[index]);
+        }
+
+        cache[index][cacheKey] = ans;
+
+        return ans;
+    }
+}
---
+++ LeetCodeExercise.Tests/DynamicProgramming/Exercise309Tests.cs
@@ -0,0 +1,36 @@
+﻿using LeetCodeExercise.Core.DynamicProgramming;
+
+namespace LeetCodeExercise.Tests.DynamicProgramming;
+
+public class Exercise309Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int[] prices, int expectedResult)
+    {
+        int result = Exercise309.MaxProfit(prices);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            new[] { 1, 2, 3, 0, 2 },
+            3
+        ];
+
+        yield return
+        [
+            new[] { 1 },
+            0
+        ];
+
+        yield return
+        [
+            new[] { 1, 2, 4 },
+            3
+        ];
+    }
+}
