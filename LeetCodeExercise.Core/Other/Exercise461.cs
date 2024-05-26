a9f036236578c626ba93b449d2f91035b002dcb3
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-26 18:50:20 +0200

    feat: exercise 461

---
+++ LeetCodeExercise.Core/Other/Exercise461.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCodeExercise.Core.Other;
+
+public static class Exercise461
+{
+    public static int HammingDistance(int x, int y)
+    {
+        int result = x ^ y;
+        int response = Convert.ToString(Convert.ToInt32(result.ToString(), 10), 2).ToCharArray().Count(x => x == '1');
+
+        return response;
+    }
+}
---
+++ LeetCodeExercise.Tests/Other/Exercise461Tests.cs
@@ -0,0 +1,32 @@
+﻿using LeetCodeExercise.Core.Other;
+
+namespace LeetCodeExercise.Tests.Other;
+
+public class Exercise461Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int x, int y, int expectedResult)
+    {
+        int result = Exercise461.HammingDistance(x, y);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            1,
+            4,
+            2
+        ];
+
+        yield return
+        [
+            3,
+            1,
+            1
+        ];
+    }
+}
