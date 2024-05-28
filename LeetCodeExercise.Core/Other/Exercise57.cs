b189f835d01d84723eb0a903fe2533de9a6ddc5c
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-05-28 20:19:22 +0200

    feat: exercise 57

---
+++ LeetCodeExercise.Core/Other/Exercise57.cs
@@ -0,0 +1,88 @@
+﻿namespace LeetCodeExercise.Core.Other;
+
+public static class Exercise57
+{
+    public static int[][] Insert(int[][] intervals, int[] newInterval)
+    {
+        List<int[]> updatedIntervals = new();
+        if (intervals.Length == 0)
+        {
+            updatedIntervals.Add(newInterval);
+
+            return updatedIntervals.ToArray();
+        }
+
+        if (newInterval[1] < intervals[0][0])
+        {
+            updatedIntervals.Add(newInterval);
+            updatedIntervals.AddRange(intervals);
+
+            return updatedIntervals.ToArray();
+        }
+
+        for (int i = 0; i < intervals.Length; i++)
+        {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            if (i == 0)
+            {
+                if (newInterval[0] < start)
+                {
+                    start = newInterval[0];
+                }
+
+                if (newInterval[0] <= end && newInterval[1] > end)
+                {
+                    end = newInterval[1];
+                }
+
+                updatedIntervals.Add([start, end]);
+
+                continue;
+            }
+
+            int previousIndex = updatedIntervals.Count - 1;
+            if (newInterval[0] > updatedIntervals[previousIndex][1]
+                && newInterval[0] < intervals[i][0]
+                && newInterval[1] > intervals[i][0])
+            {
+                start = newInterval[0];
+            }
+
+            if (newInterval[0] <= end && newInterval[1] > end)
+            {
+                end = newInterval[1];
+            }
+
+            if (updatedIntervals[previousIndex][1] >= start)
+            {
+                updatedIntervals[previousIndex][1] = end;
+
+                continue;
+            }
+
+            if (newInterval[0] > updatedIntervals[previousIndex][1] && newInterval[1] < intervals[i][0])
+            {
+                updatedIntervals.Add(newInterval);
+            }
+
+            updatedIntervals.Add([start, end]);
+        }
+
+        if (updatedIntervals.Count <= 0)
+        {
+            return updatedIntervals.ToArray();
+        }
+
+        if (newInterval[0] <= updatedIntervals[^1][1] && newInterval[1] > updatedIntervals[^1][1])
+        {
+            updatedIntervals[^1][1] = newInterval[1];
+        }
+        else if (newInterval[0] > updatedIntervals[^1][1] && newInterval[1] > updatedIntervals[^1][1])
+        {
+            updatedIntervals.Add(newInterval);
+        }
+
+        return updatedIntervals.ToArray();
+    }
+}
---
+++ LeetCodeExercise.Tests/Other/Exercise57Tests.cs
@@ -0,0 +1,56 @@
+﻿using LeetCodeExercise.Core.Other;
+
+namespace LeetCodeExercise.Tests.Other;
+
+public class Exercise57Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int[][] intervals, int[] newInterval, int[][] expectedResult)
+    {
+        int[][] result = Exercise57.Insert(intervals, newInterval);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            new int[][] { new int[] { 1, 3 }, new int[] { 6, 9 } },
+            new int[] { 2, 5 },
+            new int[][] { new int[] { 1, 5 }, new int[] { 6, 9 } }
+        ];
+
+        yield return
+        [
+            new int[][]
+            {
+                new int[] { 1, 2 }, new int[] { 3, 5 }, new int[] { 6, 7 }, new int[] { 8, 10 }, new int[] { 12, 16 }
+            },
+            new int[] { 4, 8 },
+            new int[][] { new int[] { 1, 2 }, new int[] { 3, 10 }, new int[] { 12, 16 } }
+        ];
+
+        yield return
+        [
+            new int[][] { },
+            new int[] { 5, 7 },
+            new int[][] { new int[] { 5, 7 } }
+        ];
+
+        yield return
+        [
+            new int[][] { new int[] { 1, 5 } },
+            new int[] { 5, 7 },
+            new int[][] { new int[] { 1, 7 } }
+        ];
+
+        yield return
+        [
+            new int[][] { new int[] { 1, 5 } },
+            new int[] { 6, 8 },
+            new int[][] { new int[] { 1, 5 }, new int[] { 6, 8 } }
+        ];
+    }
+}
