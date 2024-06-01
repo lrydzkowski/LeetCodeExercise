9605f307ec120f16ba772058bbfc0624b5afdf75
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-06-01 19:27:46 +0200

    feat: exercise 787

---
+++ LeetCodeExercise.Core/Other/Exercise787.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCodeExercise.Core.Other;
+
+public static class Exercise787
+{
+    public static int FindCheapestPrice(int n, int[][] flights, int src, int dst, int k)
+    {
+        Dictionary<int, List<List<int>>> graph = new();
+        foreach (int[] flight in flights)
+        {
+            graph.TryAdd(flight[0], []);
+            graph[flight[0]].Add([flight[1], flight[2]]);
+        }
+
+        List<int> prices = new();
+        for (int i = 0; i < n; i++)
+        {
+            prices.Add(int.MaxValue);
+        }
+
+        prices[src] = 0;
+        PriorityQueue<List<int>, int> heap = new();
+        heap.Enqueue([src, 0, 1], 1);
+
+        while (heap.Count > 0)
+        {
+            List<int> curr = heap.Dequeue();
+            int node = curr[0];
+            int currPrice = curr[1];
+            int numberOfEdges = curr[2];
+
+            if (numberOfEdges >= k + 2 || !graph.ContainsKey(node))
+            {
+                continue;
+            }
+
+            foreach (List<int> edge in graph[node])
+            {
+                int neighbor = edge[0];
+                int price = edge[1];
+                int cost = currPrice + price;
+                if (cost < prices[neighbor])
+                {
+                    prices[neighbor] = cost;
+                    heap.Enqueue([neighbor, cost, numberOfEdges + 1], numberOfEdges + 1);
+                }
+            }
+        }
+
+        return prices[dst] < int.MaxValue ? prices[dst] : -1;
+    }
+}
---
+++ LeetCodeExercise.Tests/Other/Exercise787Tests.cs
@@ -0,0 +1,98 @@
+﻿using LeetCodeExercise.Core.Other;
+
+namespace LeetCodeExercise.Tests.Other;
+
+public class Exercise787Tests
+{
+    [Theory]
+    [MemberData(nameof(GetParameters))]
+    public void Test(int n, int[][] flights, int src, int dest, int k, int expectedResult)
+    {
+        int result = Exercise787.FindCheapestPrice(n, flights, src, dest, k);
+
+        Assert.Equal(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetParameters()
+    {
+        yield return
+        [
+            4,
+            new int[][]
+            {
+                new int[] { 0, 1, 100 }, new int[] { 1, 2, 100 }, new int[] { 2, 0, 100 }, new int[] { 1, 3, 600 },
+                new int[] { 2, 3, 200 }
+            },
+            0,
+            3,
+            1,
+            700
+        ];
+
+        yield return
+        [
+            3,
+            new int[][]
+            {
+                new int[] { 0, 1, 100 }, new int[] { 1, 2, 100 }, new int[] { 0, 2, 500 }
+            },
+            0,
+            2,
+            1,
+            200
+        ];
+
+        yield return
+        [
+            3,
+            new int[][]
+            {
+                new int[] { 0, 1, 100 }, new int[] { 1, 2, 100 }, new int[] { 0, 2, 500 }
+            },
+            0,
+            2,
+            0,
+            500
+        ];
+
+        yield return
+        [
+            2,
+            new int[][]
+            {
+                new int[] { 0, 1, 2 }
+            },
+            1,
+            0,
+            0,
+            -1
+        ];
+
+        yield return
+        [
+            4,
+            new int[][]
+            {
+                new int[] { 0, 1, 1 }, new int[] { 0, 2, 5 }, new int[] { 1, 2, 1 }, new int[] { 2, 3, 1 }
+            },
+            0,
+            3,
+            1,
+            6
+        ];
+
+        yield return
+        [
+            5,
+            new int[][]
+            {
+                new int[] { 0, 1, 5 }, new int[] { 1, 2, 5 }, new int[] { 0, 3, 2 }, new int[] { 3, 1, 2 },
+                new int[] { 1, 4, 1 }, new int[] { 4, 2, 1 }
+            },
+            0,
+            2,
+            2,
+            7
+        ];
+    }
+}
